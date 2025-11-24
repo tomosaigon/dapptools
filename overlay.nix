1aa1a8da7ff87cdef2c4eb9add273be1dc00863b
tomo <tomo@crowdeo.com>
2025-11-24 18:02:55 +0700

    fix semver-range

--- overlay.nix
+++ overlay.nix
@@ -8,19 +8,20 @@ in rec {
   dapptoolsSrc = self.callPackage (import ./nix/dapptools-src.nix) {};
 
    haskellPackages =
-     super.haskellPackages.override (old: {
-     overrides = lib.composeExtensions (old.overrides or (_: _: {})) [
-       (self-hs: super-hs: {
-         semver-range = super-hs.semver-range.override {
-           src = super-hs.fetchurl {
-             url = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
-             sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
-           };
-         };
-       })
-       (import ./haskell.nix { inherit lib; pkgs = self;})
-     ];
-   });
+    super.haskellPackages.override (old: {
+      overrides = lib.composeExtensions
+        (lib.composeExtensions
+          (old.overrides or (_: _: {}))
+          (import ./haskell.nix { inherit lib; pkgs = self;}))
+        (self-hs: super-hs: {
+          semver-range = super-hs.semver-range.override {
+            src = super-hs.fetchurl {
+              url = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
+              sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
+            };
+          };
+        });
+    });
 
 
   unwrappedHaskellPackages =
