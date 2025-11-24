ee1be0857d389500a8fdf6063d81ddf43613e64e
tomo <tomo@crowdeo.com>
2025-11-24 17:51:50 +0700

    fork for makerpkgs

--- overlay.nix
+++ overlay.nix
@@ -7,12 +7,21 @@ let
 in rec {
   dapptoolsSrc = self.callPackage (import ./nix/dapptools-src.nix) {};
 
-  haskellPackages =
-    super.haskellPackages.override (old: {
-    overrides = lib.composeExtensions (old.overrides or (_: _: {})) (
-      import ./haskell.nix { inherit lib; pkgs = self;}
-    );
-  });
+   haskellPackages =
+     super.haskellPackages.override (old: {
+     overrides = lib.composeExtensions (old.overrides or (_: _: {})) [
+       (self-hs: super-hs: {
+         semver-range = super-hs.semver-range.override {
+           src = super-hs.fetchurl {
+             url = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
+             sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
+           };
+         };
+       })
+       (import ./haskell.nix { inherit lib; pkgs = self;})
+     ];
+   });
+
 
   unwrappedHaskellPackages =
     super.haskellPackages.override (old: {
