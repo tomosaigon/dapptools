37f0796ef8c2daadf2924511b453ac00125b4cae
tomo <tomo@crowdeo.com>
2025-11-28 10:39:51 +0700

    Pin semver-range from Hackage for dapp 0.31.1 build

--- overlay.nix
+++ overlay.nix
@@ -7,11 +7,21 @@ let
 in rec {
   dapptoolsSrc = self.callPackage (import ./nix/dapptools-src.nix) {};
 
-  haskellPackages = super.haskellPackages.override (old: {
-    overrides = lib.composeExtensions (old.overrides or (_: _: {})) (
-      import ./haskell.nix { inherit lib; pkgs = self; }
-    );
-  });
+  haskellPackages =
+    super.haskellPackages.override (old: {
+      overrides = lib.composeExtensions
+        (lib.composeExtensions
+          (old.overrides or (_: _: {}))
+          (import ./haskell.nix { inherit lib; pkgs = self; }))
+        (self-hs: super-hs: {
+          semver-range = (super-hs.semver-range.override {}).overrideAttrs (oldAttrs: {
+            src = self.pkgs.fetchurl {
+              url = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
+              sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
+            };
+          });
+        });
+    });
 
   solidityPackage = import ./nix/solidity-package.nix {
     inherit (self) pkgs;
